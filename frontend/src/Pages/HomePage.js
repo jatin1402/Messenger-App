411b53d592b5cfbe829b44914ddb94bf0be82194
jatinzop <jatin.varshney@zopsmart.com>
2022-08-10 01:55:07 +0530

    changes

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -7,52 +7,52 @@ import Login from '../components/Authentication/Login';
 import Signup from '../components/Authentication/Signup';
 
 export default function HomePage() {
-    const history = useNavigate();
-    useEffect(() => {
-        // if user is already logged in
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-        if (userInfo) {
-            history('/chats');
-        }
-    }, [history]);
-    return (
-        <Container w="xl" centerContent>
-            <Box
-                d="flex"
-                justifyContent="center"
-                justifySelf={'center'}
-                p={2.5}
-                bg={'white'}
-                w="100%"
-                m="40px 0 15px 0"
-                borderRadius="lg"
-                borderWidth="1px"
-            >
-                <Text
-                    marginLeft={'170px'}
-                    fontSize={'3xl'}
-                    fontFamily={'Work sans'}
-                    color="black"
-                >
-                    Talk-a-tive
-                </Text>
-            </Box>
-            <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-                <Tabs isFitted variant="soft-rounded">
-                    <TabList>
-                        <Tab>Login</Tab>
-                        <Tab>SignUp</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <Login />
-                        </TabPanel>
-                        <TabPanel>
-                            <Signup />
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Box>
-        </Container>
-    );
+  const history = useNavigate();
+  useEffect(() => {
+    // if user is already logged in
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    if (userInfo) {
+      history('/chats');
+    }
+  }, [history]);
+  return (
+    <Container w="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        justifySelf={'center'}
+        p={2.5}
+        bg={'white'}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          marginLeft={'170px'}
+          fontSize={'3xl'}
+          fontFamily={'Work sans'}
+          color="black"
+        >
+          Talk-a-tive
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList>
+            <Tab>Login</Tab>
+            <Tab>SignUp</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
